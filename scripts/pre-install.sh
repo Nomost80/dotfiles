f4340718105bd3ea26d97019a7dc6a113d8edb26
Guillaume Soufflet <guillaume.soufflet1@viacesi.fr>
2019-12-23 12:36:47 +0100

    pre-install script fixed

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -1,12 +1,16 @@
 sudo pacman -Syu --noconfirm
 
 # Install packages from official repositories
-sudo pacman -S --noconfirm yay git curl kcoreaddons kdeclarative plasma-framework kdecoration qt5-declarative qt5-x11extras cmake extra-cmake-modules kvantum-manjaro
+sudo pacman -S --noconfirm yay git curl kcoreaddons kdeclarative plasma-framework kdecoration qt5-declarative qt5-x11extras make extra-cmake-modules kvantum-manjaro
 
 # Install packages from AUR
 yay -S --noconfirm sierrabreeze-kwin-decoration-git bitwarden balena-etcher gtkhash
 
 cd ~/Downloads
 # Install window buttons widget
-git clone https://github.com/psifidotos/applet-window-buttons.git && ./applet-window-buttons/install.sh
-rm -rf *
+git clone https://github.com/psifidotos/applet-window-buttons.git && cd applet-window-buttons 
+sh applet-window-buttons/install.sh
+
+# Clean
+cd ~/Downloads
+rm -rf applet-window-buttons
